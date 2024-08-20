7edb88f9de49908f1bd906dbcc5eaf7c80dfcc4f
APIVerve Team <hello@apiverve.com>
2024-08-20 13:35:08 -0500

    Bug Fixes

---
+++ WebsitetoPDFQueryOptions.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class WebsitetoPDFQueryOptions {
+public string url { get; set; }
+public double marginTop { get; set; }
+public double marginRight { get; set; }
+public double marginBottom { get; set; }
+public double marginLeft { get; set; }
+}
+
+}
